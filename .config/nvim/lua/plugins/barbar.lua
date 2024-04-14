b5df232061f3281b96b9c7fcefc670bc74a8c580
swahpy <hitwhy@gmail.com>
2024-04-14 22:19:13 +0800

    add plugin barbar

--- .config/nvim/lua/plugins/barbar.lua
+++ .config/nvim/lua/plugins/barbar.lua
@@ -1,15 +1,26 @@
 return {
 	'romgrk/barbar.nvim',
+  version = '^1.0.0', -- optional: only update when a new 1.x version is released
   dependencies = {
     'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
     'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
   },
   init = function() vim.g.barbar_auto_setup = false end,
   opts = {
-    -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-    -- animation = true,
-    -- insert_at_start = true,
-    -- …etc.
+    focus_on_close = "previous",
+    icons = {
+      buffer_index = true,
+      buffer_number = "subscript",
+      present = "powerline",
+    },
+    -- Comment below because I use nvim-tree in float mode.
+    -- If you set it to sidebar, then enable below config.
+		-- sidebar_filetypes = {
+		-- 	-- Use the default values: {event = 'BufWinLeave', text = '', align = 'left'}
+		-- 	NvimTree = true,
+		-- },
   },
-  version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  keys = {
+
+  },
 }
