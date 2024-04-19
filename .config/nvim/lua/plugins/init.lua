bf41ea4b9121bd4dbd3c84df4885c003a4f20a1f
swahpy <hitwhy@gmail.com>
2024-04-19 06:07:53 +0000

    add plugin tree-sitter-pairs

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,4 +1,5 @@
 return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugis use
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  "yorickpeterse/nvim-tree-pairs", -- to fix % jump with tree-sitter
 }
