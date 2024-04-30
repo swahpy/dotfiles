746c8c725fad2d571cc59fb5d01d23b980bba6f9
swahpy <hitwhy@gmail.com>
2024-04-30 10:25:49 +0800

    remove callbacks setup for obsidian

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -22,6 +22,22 @@ return {
         name = "personal",
         path = "~/Documents/obsidian/wanghy/",
       },
+      {
+        name = "no-vault",
+        path = function()
+          -- alternatively use the CWD:
+          -- return assert(vim.fn.getcwd())
+          return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+        end,
+        overrides = {
+          notes_subdir = vim.NIL,  -- have to use 'vim.NIL' instead of 'nil'
+          new_notes_location = "current_dir",
+          templates = {
+            subdir = vim.NIL,
+          },
+          disable_frontmatter = true,
+        },
+      },
     },
 
     -- Optional, if you keep notes in a specific subdirectory of your vault.
@@ -192,33 +208,6 @@ return {
     -- 3. "hsplit" - to open in a horizontal split if there's not already a horizontal split
     open_notes_in = "current",
 
-    -- Optional, define your own callbacks to further customize behavior.
-    callbacks = {
-      -- Runs at the end of `require("obsidian").setup()`.
-      ---@param client obsidian.Client
-      post_setup = function(client) end,
-
-      -- Runs anytime you enter the buffer for a note.
-      ---@param client obsidian.Client
-      ---@param note obsidian.Note
-      enter_note = function(client, note) end,
-
-      -- Runs anytime you leave the buffer for a note.
-      ---@param client obsidian.Client
-      ---@param note obsidian.Note
-      leave_note = function(client, note) end,
-
-      -- Runs right before writing the buffer for a note.
-      ---@param client obsidian.Client
-      ---@param note obsidian.Note
-      pre_write_note = function(client, note) end,
-
-      -- Runs anytime the workspace is set/changed.
-      ---@param client obsidian.Client
-      ---@param workspace obsidian.Workspace
-      post_set_workspace = function(client, workspace) end,
-    },
-
     -- Optional, configure additional syntax highlighting / extmarks.
     -- This requires you have `conceallevel` set to 1 or 2. See `:help conceallevel` for more details.
     ui = {
@@ -280,7 +269,7 @@ return {
     },
   },
   config = function(_, opts)
-    vim.opt.conceallevel = 1
+    vim.opt.conceallevel = 2
     local obs = require("obsidian")
     obs.setup(opts)
     -- setup keymaps
