c0df036ec60c33df430cd7915b21755a6ebe6a6c
swahpy <hitwhy@gmail.com>
2024-04-30 22:21:09 +0800

    format adjust

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
   'nvim-telescope/telescope.nvim',
   branch = '0.1.x',
-  dependencies = { 
+  dependencies = {
     'nvim-lua/plenary.nvim',
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
