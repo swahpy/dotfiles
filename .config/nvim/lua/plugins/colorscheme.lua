fbd55536cb3eb260afc11bd03fb6b5333f7e0c92
swahpy <hitwhy@gmail.com>
2024-05-08 22:21:56 +0800

    try theme cyberbream

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,35 @@
 return {
-  'uloco/bluloco.nvim',
-  lazy = false,
-  priority = 1000,
-  dependencies = { 'rktjmp/lush.nvim' },
-  config = function()
-    require("bluloco").setup({
-      style = "light", -- "auto" | "dark" | "light"
-      transparent = false,
-      italics = false,
-      terminal = vim.fn.has("gui_running") == 1, -- bluoco colors are enabled in gui terminals per default.
-      guicursor   = true,
-    })
-    vim.opt.termguicolors = true
-    vim.cmd('colorscheme bluloco')
-  end,
+  {
+    'uloco/bluloco.nvim',
+    enbaled = false,
+    lazy = false,
+    priority = 1000,
+    dependencies = { 'rktjmp/lush.nvim' },
+    config = function()
+      require("bluloco").setup({
+        style = "light", -- "auto" | "dark" | "light"
+        transparent = false,
+        italics = false,
+        terminal = vim.fn.has("gui_running") == 1, -- bluoco colors are enabled in gui terminals per default.
+        guicursor   = true,
+      })
+      vim.opt.termguicolors = true
+      vim.cmd('colorscheme bluloco')
+    end,
+  },
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("cyberdream").setup({
+        transparent = true,
+        italic_comments = true,
+        hide_fillchars = true,
+        borderless_telescope = true,
+        terminal_colors = true,
+      })
+      vim.cmd("colorscheme cyberdream") -- set the colorscheme
+    end,
+  },
 }
