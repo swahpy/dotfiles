eab6c2db3d076df6be03414d8852be96008ea2a7
swahpy <hitwhy@gmail.com>
2024-05-16 16:51:20 +0800

    setup conform to format on save

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,6 +1,5 @@
 return {
 	"stevearc/conform.nvim",
-	enabled = true,
 	init = function()
 		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 	end,
@@ -21,12 +20,20 @@ return {
 		log_level = vim.log.levels.ERROR,
 		-- Conform will notify you when a formatter errors
 		notify_on_error = true,
+		format_on_save = {
+			-- These options will be passed to conform.format()
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
 	},
 	keys = {
 		{
 			"<leader><leader>f",
 			function()
-				require("conform").format({ async = true, lsp_fallback = true })
+				require("conform").format({
+					timeout_ms = 500,
+					lsp_fallback = true,
+				})
 			end,
 			mode = { "n", "v" },
 			desc = "Format buffer",
