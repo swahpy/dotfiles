820dff0f6170ccc2487c57d248bd52ece3d19dab
swahpy <hitwhy@gmail.com>
2024-05-16 13:59:50 +0800

    switch join split to treesj

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,14 +1,23 @@
 return {
-  "nvim-lua/plenary.nvim",         -- lua functions that many plugis use
+  "nvim-lua/plenary.nvim",          -- lua functions that many plugis use
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
-  "yorickpeterse/nvim-tree-pairs", -- to fix % jump with tree-sitter
+  "yorickpeterse/nvim-tree-pairs",  -- to fix % jump with tree-sitter
   "RRethy/nvim-treesitter-endwise",
-  { "numToStr/Comment.nvim", event = "VeryLazy", opts = { ignore = "^$" } },
   {
-    "AndrewRadev/splitjoin.vim",
+    "numToStr/Comment.nvim",
+    event = "VeryLazy",
+    opts = { ignore = "^$" },
+  },
+  {
+    "Wansmer/treesj",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("treesj").setup({ --[[ your config ]] })
+    end,
     keys = {
-      { "<leader><leader>s", "<cmd>SplitjoinSplit<cr>", desc = "[S]plit current line" },
-      { "<leader><leader>j", "<cmd>SplitjoinJoin<cr>",  desc = "[J]oin current line" },
+      { "<leader><leader>t", "<cmd>TSJToggle<cr>", desc = "[T]oggle join and split" },
+      { "<leader><leader>j", "<cmd>TSJJoin<cr>",   desc = "[J]oin node under cursor" },
+      { "<leader><leader>s", "<cmd>TSJSplit<cr>",  desc = "[S]plit node under cursor" },
     },
   },
   {
