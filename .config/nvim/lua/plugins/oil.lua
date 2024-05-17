06bb2e49b7366a82e344a49ad83e73001fbb24af
swahpy <hitwhy@gmail.com>
2024-05-17 10:53:04 +0800

    format oil

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,33 +1,35 @@
 return {
-  'stevearc/oil.nvim',
-  -- Optional dependencies
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    -- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
-    delete_to_trash = true,
-    -- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
-    -- options with a `callback` (e.g. { callback = function() ... end, desc = "", mode = "n" })
-    -- Additionally, if it is a string that matches "actions.<name>",
-    -- it will use the mapping at require("oil.actions").<name>
-    -- Set to `false` to remove a keymap
-    -- See :help oil-actions for a list of all available actions
-    keymaps = {
-      ["?"] = "actions.show_help",
-      ["q"] = "actions.close",
-      ["<F5>"] = "actions.refresh",
-      ["<C-v>"] = "actions.select_vsplit",
-      ["<C-l>"] = false,
-      ["<C-s>"] = false,
-    },
-    -- Skip the confirmation popup for simple operations (:help oil.skip_confirm_for_simple_edits)
-    skip_confirm_for_simple_edits = true,
-  },
-  config = function (_, opts)
-    local oil = require("oil")
-    oil.setup(opts)
+	"stevearc/oil.nvim",
+	-- Optional dependencies
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		-- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
+		delete_to_trash = true,
+		-- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
+		-- options with a `callback` (e.g. { callback = function() ... end, desc = "", mode = "n" })
+		-- Additionally, if it is a string that matches "actions.<name>",
+		-- it will use the mapping at require("oil.actions").<name>
+		-- Set to `false` to remove a keymap
+		-- See :help oil-actions for a list of all available actions
+		keymaps = {
+			["?"] = "actions.show_help",
+			["q"] = "actions.close",
+			["<F5>"] = "actions.refresh",
+			["<C-v>"] = "actions.select_vsplit",
+			["<C-x>"] = "actions.select_split",
+			["<C-h>"] = false,
+			["<C-l>"] = false,
+			["<C-s>"] = false,
+		},
+		-- Skip the confirmation popup for simple operations (:help oil.skip_confirm_for_simple_edits)
+		skip_confirm_for_simple_edits = true,
+	},
+	config = function(_, opts)
+		local oil = require("oil")
+		oil.setup(opts)
 
-    --> setup keymaps for oil
-    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-    vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-  end
+		--> setup keymaps for oil
+		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+		vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+	end,
 }
