0f3f5439cf4fbe03bcae60fa480e1f9a4d85cb51
swahpy <hitwhy@gmail.com>
2024-05-18 22:17:57 +0800

    bug fix

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,109 +1,109 @@
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify",
-  },
-  opts = {
-    lsp = {
-      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-      },
-    },
-    presets = {
-      bottom_search = true, -- use a classic bottom cmdline for search
-      command_palette = false, -- position the cmdline and popupmenu together
-      long_message_to_split = true, -- long messages will be sent to a split
-      inc_rename = true, -- enables an input dialog for inc-rename.nvim
-      lsp_doc_border = false, -- add a border to hover docs and signature help
-    },
-    commands = {
-      history = {
-        view = "popup",
-      },
-      warnings = {
-        -- options for the message history that you get with `:Noice`
-        view = "popup",
-        opts = { enter = true, format = "details" },
-        filter = { warning = true },
-        filter_opts = { reverse = true },
-      },
-    },
-    routes = {
-      {
-        view = "split",
-        filter = { event = "msg_show", min_height = 10 },
-      },
-    },
-  },
-  config = function(_, opts)
-    local noice = require("noice")
-    noice.setup(opts)
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
+	opts = {
+		lsp = {
+			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+			},
+		},
+		presets = {
+			bottom_search = true, -- use a classic bottom cmdline for search
+			command_palette = false, -- position the cmdline and popupmenu together
+			long_message_to_split = false, -- long messages will be sent to a split
+			inc_rename = true, -- enables an input dialog for inc-rename.nvim
+			lsp_doc_border = false, -- add a border to hover docs and signature help
+		},
+		commands = {
+			history = {
+				view = "popup",
+			},
+			warnings = {
+				-- options for the message history that you get with `:Noice`
+				view = "popup",
+				opts = { enter = true, format = "details" },
+				filter = { warning = true },
+				filter_opts = { reverse = true },
+			},
+		},
+		routes = {
+			{
+				filter = {
+					event = "msg_show",
+					kind = "",
+					find = "written",
+				},
+				opts = { skip = true },
+			},
+		},
+	},
+	config = function(_, opts)
+		local noice = require("noice")
+		noice.setup(opts)
 
-    -- setup keymaps for noice
-    local wk = require("which-key")
-    wk.register({
-      ["<leader>n"] = {
-        name = "+noice",
-        a = {
-          "<cmd>Noice<cr>",
-          "Show message history",
-        },
-        l = {
-          "<cmd>NoiceLast<cr>",
-          "Show last message",
-        },
-        d = {
-          "<cmd>NoiceDismiss<cr>",
-          "Dismiss all messages",
-        },
-        e = {
-          "<cmd>NoiceErrors<cr>",
-          "Show error messages",
-        },
-        D = {
-          "<cmd>NoiceDisable<cr>",
-          "[D]isable noice",
-        },
-        E = {
-          "<cmd>NoiceEnable<cr>",
-          "[E]nable noice",
-        },
-        s = {
-          "<cmd>NoiceStats<cr>",
-          "Show debugging stats",
-        },
-        w = {
-          "<cmd>NoiceWarnings<cr>",
-          "Show warning messages",
-        },
-      },
-    })
-    vim.keymap.set({ "n", "i", "s" }, "<A-u>", function()
-      if not require("noice.lsp").scroll(4) then
-        return "<A-u>"
-      end
-    end, { silent = true, expr = true })
+		-- setup keymaps for noice
+		local wk = require("which-key")
+		wk.register({
+			["<leader>n"] = {
+				name = "+noice",
+				a = {
+					"<cmd>Noice<cr>",
+					"Show message history",
+				},
+				l = {
+					"<cmd>NoiceLast<cr>",
+					"Show last message",
+				},
+				d = {
+					"<cmd>NoiceDismiss<cr>",
+					"Dismiss all messages",
+				},
+				e = {
+					"<cmd>NoiceErrors<cr>",
+					"Show error messages",
+				},
+				D = {
+					"<cmd>NoiceDisable<cr>",
+					"[D]isable noice",
+				},
+				E = {
+					"<cmd>NoiceEnable<cr>",
+					"[E]nable noice",
+				},
+				s = {
+					"<cmd>NoiceStats<cr>",
+					"Show debugging stats",
+				},
+				w = {
+					"<cmd>NoiceWarnings<cr>",
+					"Show warning messages",
+				},
+			},
+		})
+		vim.keymap.set({ "n", "i", "s" }, "<A-u>", function()
+			if not require("noice.lsp").scroll(4) then
+				return "<A-u>"
+			end
+		end, { silent = true, expr = true })
 
-    vim.keymap.set({ "n", "i", "s" }, "<A-d>", function()
-      if not require("noice.lsp").scroll(-4) then
-        return "<A-d>"
-      end
-    end, { silent = true, expr = true })
+		vim.keymap.set({ "n", "i", "s" }, "<A-d>", function()
+			if not require("noice.lsp").scroll(-4) then
+				return "<A-d>"
+			end
+		end, { silent = true, expr = true })
 
-    --> setup timeout
-    require("notify").setup({
-      timeout = 1000,
-      stages = "fade",
-      render = "wrapped-compact",
-    })
-  end
+		--> setup timeout
+		require("notify").setup({
+			timeout = 1500,
+			stages = "fade",
+			render = "wrapped-compact",
+		})
+	end,
 }
