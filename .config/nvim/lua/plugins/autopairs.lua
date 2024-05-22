89c6021449ef20ecb98e2b57864943b2d3ef5f4e
swahpy <hitwhy@gmail.com>
2024-05-22 19:29:33 +0800

    replace autopairs with mini pairs

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -1,23 +1,24 @@
 return {
-  'windwp/nvim-autopairs',
-  event = "InsertEnter",
-  opts = {
-    check_ts = true,
-    ts_config = {
-      lua = {'string'},-- it will not add a pair on that treesitter node
-      javascript = {'template_string'},
-      java = false,-- don't check treesitter on java
-    }
-  },
-  config = function(_, opts)
-    -- import nvim-autopairs completion functionality
-    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-    -- import nvim-cmp plugin (completions plugin)
-    local cmp = require("cmp")
-    -- make autopairs and completion work together
-    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-    -- setup autopairs
-    local ap = require("nvim-autopairs")
-    ap.setup(opts)
-  end
+	"windwp/nvim-autopairs",
+	enabled = false,
+	event = "InsertEnter",
+	opts = {
+		check_ts = true,
+		ts_config = {
+			lua = { "string" }, -- it will not add a pair on that treesitter node
+			javascript = { "template_string" },
+			java = false, -- don't check treesitter on java
+		},
+	},
+	config = function(_, opts)
+		-- import nvim-autopairs completion functionality
+		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+		-- import nvim-cmp plugin (completions plugin)
+		local cmp = require("cmp")
+		-- make autopairs and completion work together
+		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		-- setup autopairs
+		local ap = require("nvim-autopairs")
+		ap.setup(opts)
+	end,
 }
---
+++ .config/nvim/lua/plugins/mini/pairs.lua
@@ -0,0 +1,13 @@
+return {
+	"echasnovski/mini.pairs",
+	version = false,
+	config = function()
+		require("mini.pairs").setup({
+			modes = { insert = true, command = true, terminal = false },
+			mappings = {
+				["<"] = { action = "open", pair = "<>", neigh_pattern = "[^\\]." },
+				["}"] = { action = "close", pair = "<>", neigh_pattern = "[^\\]." },
+			},
+		})
+	end,
+}
