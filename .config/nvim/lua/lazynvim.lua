8380b4d40d93da49d49f85a8f482363d56c96a67
swahpy <hitwhy@gmail.com>
2024-05-22 13:38:31 +0800

    integrate mini plugins

--- .config/nvim/lua/lazynvim.lua
+++ .config/nvim/lua/lazynvim.lua
@@ -1,27 +1,26 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+---@diagnostic disable-next-line:undefined-field
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    -- gnutls_handshake() failed: The TLS connection was non-properly terminated.
-    -- above error may occur because of network issues, then switch to git url.
-    -- "git@github.com:folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		-- gnutls_handshake() failed: The TLS connection was non-properly terminated.
+		-- above error may occur because of network issues, then switch to git url.
+		-- "git@github.com:folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup(
-  {
-    { import = "plugins" },
-  },
-  {
-    change_detection = {
-      -- automatically check for config file changes and reload the ui
-      enabled = true,
-      notify = false, -- get a notification when changes are found
-    },
-  }
-)
+require("lazy").setup({
+	{ import = "plugins" },
+	{ import = "plugins.mini" },
+}, {
+	change_detection = {
+		-- automatically check for config file changes and reload the ui
+		enabled = true,
+		notify = false, -- get a notification when changes are found
+	},
+})
