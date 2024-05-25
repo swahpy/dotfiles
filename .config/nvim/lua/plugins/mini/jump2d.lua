b0291a547be588acdfa79e4287c47d7a1af95e2f
swahpy <hitwhy@gmail.com>
2024-05-25 20:40:09 +0800

    add mini plugin jump2d

--- .config/nvim/lua/plugins/mini/jump2d.lua
+++ .config/nvim/lua/plugins/mini/jump2d.lua
@@ -3,11 +3,6 @@ return {
 	version = false,
 	config = function()
 		local jump2d = require("mini.jump2d")
-		jump2d.setup({
-			-- Module mappings. Use `''` (empty string) to disable one.
-			mappings = {
-				start_jumping = "gw",
-			},
-		})
+		jump2d.setup({})
 	end,
 }
