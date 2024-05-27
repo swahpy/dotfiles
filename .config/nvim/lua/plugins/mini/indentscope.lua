40b747eb71a778bf02ddda5c6201f8525d704796
swahpy <hitwhy@gmail.com>
2024-05-27 15:38:48 +0800

    replace indent-blankline with mini.indentscope

---
+++ .config/nvim/lua/plugins/mini/indentscope.lua
@@ -0,0 +1,25 @@
+return {
+	"echasnovski/mini.indentscope",
+	version = false,
+	config = function()
+		local indent = require("mini.indentscope")
+		indent.setup({})
+		-- Disable for certain filetypes
+		vim.api.nvim_create_autocmd({ "FileType" }, {
+			desc = "Disable indentscope for certain filetypes",
+			callback = function()
+				local ignore_filetypes = {
+					"dashboard",
+					"help",
+					"lazy",
+					"mason",
+					"notify",
+					"Trouble",
+				}
+				if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+					vim.b.miniindentscope_disable = true
+				end
+			end,
+		})
+	end,
+}
