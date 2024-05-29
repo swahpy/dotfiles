ec7aaafc482ac6d154211c0baab5c6d49e3847d5
swahpy <hitwhy@gmail.com>
2024-05-29 16:40:02 +0800

    add mini plugin mini.misc

---
+++ .config/nvim/lua/plugins/mini/misc.lua
@@ -0,0 +1,18 @@
+return {
+	"echasnovski/mini.misc",
+	version = false,
+	config = function()
+		local misc = require("mini.misc")
+		misc.setup({
+			-- Array of fields to make global (to be used as independent variables)
+			make_global = { "put", "put_text" },
+		})
+		misc.setup_auto_root()
+		misc.setup_restore_cursor()
+		--> keymaps
+		local map = vim.keymap.set
+		map("n", "<leader><leader>z", function()
+			misc.zoom()
+		end, { desc = "Toggle [z]oom current buffer" })
+	end,
+}
