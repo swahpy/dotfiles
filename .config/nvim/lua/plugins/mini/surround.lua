adaf757ba1f7ef7ca33fa1531e33d6f75000e6dc
swahpy <hitwhy@gmail.com>
2024-06-06 17:12:22 +0800

    remove duplicated config from mini surround

--- .config/nvim/lua/plugins/mini/surround.lua
+++ .config/nvim/lua/plugins/mini/surround.lua
@@ -4,24 +4,6 @@ return {
 	config = function()
 		local sr = require("mini.surround")
 		sr.setup({
-			-- Add custom surroundings to be used on top of builtin ones. For more
-			-- information with examples, see `:h MiniSurround.config`.
-			custom_surroundings = nil,
-
-			-- Module mappings. Use `''` (empty string) to disable one.
-			mappings = {
-				add = "sa", -- Add surrounding in Normal and Visual modes
-				delete = "sd", -- Delete surrounding
-				find = "sf", -- Find surrounding (to the right)
-				find_left = "sF", -- Find surrounding (to the left)
-				highlight = "sh", -- Highlight surrounding
-				replace = "sr", -- Replace surrounding
-				update_n_lines = "sn", -- Update `n_lines`
-
-				suffix_last = "l", -- Suffix to search with "prev" method
-				suffix_next = "n", -- Suffix to search with "next" method
-			},
-
 			-- Number of lines within which surrounding is searched
 			n_lines = 500,
 
@@ -35,9 +17,6 @@ return {
 			-- 'cover_or_nearest', 'next', 'prev', 'nearest'. For more details,
 			-- see `:h MiniSurround.config`.
 			search_method = "cover_or_next",
-
-			-- Whether to disable showing non-error feedback
-			silent = false,
 		})
 	end,
 }
