699b4b9fd42a0f0fcba8458f35f1d80d0091b39b
swahpy <hitwhy@gmail.com>
2024-06-13 17:47:13 +0800

    adjust colorscheme

--- .config/nvim/lua/plugins/highlight-colors.lua
+++ .config/nvim/lua/plugins/highlight-colors.lua
@@ -1,24 +1,28 @@
 return {
-  "brenoprata10/nvim-highlight-colors",
-  opts = {
-    ---Render style
-    ---@usage 'background'|'foreground'|'virtual'
-    render = 'background',
-    ---Set virtual symbol (requires render to be set to 'virtual')
-    virtual_symbol = '■',
-    ---Highlight named colors, e.g. 'green'
-    enable_named_colors = true,
-    ---Highlight tailwind colors, e.g. 'bg-blue-500'
-    enable_tailwind = false,
-    ---Set custom colors
-    ---Label must be properly escaped with '%' to adhere to `string.gmatch`
-    --- :help string.gmatch
-    custom_colors = {
-      { label = '%-%-theme%-primary%-color', color = '#0f1219' },
-      { label = '%-%-theme%-secondary%-color', color = '#5a5d64' },
-    },
-  },
-  keys = {
-    {"<leader>ht", "<cmd>:HighlightColors Toggle<cr>", "[T]oggle highlight"},
-  },
+	"brenoprata10/nvim-highlight-colors",
+	opts = {
+		---Render style
+		---@usage 'background'|'foreground'|'virtual'
+		render = "background",
+		---Set virtual symbol (requires render to be set to 'virtual')
+		virtual_symbol = "■",
+		---Highlight named colors, e.g. 'green'
+		enable_named_colors = true,
+		---Highlight tailwind colors, e.g. 'bg-blue-500'
+		enable_tailwind = false,
+		---Set custom colors
+		---Label must be properly escaped with '%' to adhere to `string.gmatch`
+		--- :help string.gmatch
+		custom_colors = {
+			{ label = "%-%-theme%-primary%-color", color = "#0f1219" },
+			{ label = "%-%-theme%-secondary%-color", color = "#5a5d64" },
+		},
+	},
+	config = function(_, opts)
+		local highlight = require("nvim-highlight-colors")
+		highlight.setup(opts)
+		vim.keymap.set("n", "<leader><leader>h", function()
+			highlight.toggle()
+		end, { desc = "+Toggle highlight" })
+	end,
 }
