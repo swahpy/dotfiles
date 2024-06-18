7a6437198925e26558e0e1a6508ece04b2e11061
swahpy <hitwhy@gmail.com>
2024-06-18 23:09:44 +0800

    adjust keymaps to adapt clue

--- .config/nvim/lua/plugins/vim-floaterm.lua
+++ .config/nvim/lua/plugins/vim-floaterm.lua
@@ -9,96 +9,50 @@ return {
 		vim.g.floaterm_title = "term$1/$2"
 		vim.g.floaterm_titleposition = "center"
 		--> setup floaterm keymaps
+		local nmap_leader = function(suffix, rhs, desc)
+			vim.keymap.set("n", "<Leader>" .. suffix, rhs, { desc = desc })
+		end
+		nmap_leader("tf", "<cmd>FloatermFirst<cr>", "Navigate to first floaterm")
+		nmap_leader("th", "<cmd>FloatermToggle!<cr>", "Hide all floaterm windows")
+		nmap_leader("tH", function()
+			local name = vim.fn.input("Please enter floaterm name: ")
+			vim.cmd("FloatermHide " .. name)
+		end, "Hide current floaterm window")
+		nmap_leader("tk", "<cmd>FloatermKill!<cr>", "Kill all floaterm windows")
+		nmap_leader("tK", function()
+			local name = vim.fn.input("Please enter floaterm name: ")
+			vim.cmd("FloatermKill " .. name)
+		end, "Kill current floaterm window")
+		nmap_leader("tl", "<cmd>FloatermLast<cr>", "Navigate to last floaterm")
+		nmap_leader("tn", "<cmd>FloatermNext<cr>", "Navigate to next floaterm")
+		nmap_leader("to", function()
+			local param = vim.fn.input("Please enter options and cmd:")
+			vim.cmd("FloatermNew! " .. param)
+		end, "Open a floaterm window, exec command in $SHELL")
+		nmap_leader("tO", function()
+			local param = vim.fn.input("Please enter options and cmd:")
+			vim.cmd("FloatermNew " .. param)
+		end, "+open a floaterm window")
+		nmap_leader("tp", "<cmd>FloatermPrev<cr>", "Navigate to previous floaterm")
+		nmap_leader("tr", function()
+			local cmd = vim.fn.input("Please input the command to send: ")
+			vim.cmd("FLoatermSend " .. cmd)
+		end, "Send(Run) command to floaterm")
+		nmap_leader("ts", "<cmd>FloatermShow!<cr>", "Show all floaterm windows")
+		nmap_leader("tS", function()
+			local name = vim.fn.input("Please enter floaterm name: ")
+			vim.cmd("FloatermShow " .. name)
+		end, "Show current floaterm window")
+		nmap_leader("tt", "<cmd>FloatermToggle!<cr>", "+toggle all floaterm windows")
+		nmap_leader("tT", function()
+			local name = vim.fn.input("Please enter floaterm name: ")
+			vim.cmd("FloatermToggle " .. name)
+		end, "+toggle a floaterm window")
+		nmap_leader("tu", "<cmd>FloatermUpdate<cr>", "Update floaterm window attributes")
+
 		local wk = require("which-key")
 		wk.register({
-			["<leader>t"] = {
-				name = "floaterm",
-				O = {
-					function()
-						local param = vim.fn.input("Please enter options and cmd:")
-						vim.cmd("FloatermNew " .. param)
-					end,
-					"Open a floaterm window",
-				},
-				o = {
-					function()
-						local param = vim.fn.input("Please enter options and cmd:")
-						vim.cmd("FloatermNew! " .. param)
-					end,
-					"Open a floaterm window, exec command in $SHELL",
-				},
-				p = {
-					"<cmd>FloatermPrev<cr>",
-					"Navigate to previous floaterm",
-				},
-				n = {
-					"<cmd>FloatermNext<cr>",
-					"Navigate to next floaterm",
-				},
-				f = {
-					"<cmd>FloatermFirst<cr>",
-					"Navigate to first floaterm",
-				},
-				l = {
-					"<cmd>FloatermLast<cr>",
-					"Navigate to last floaterm",
-				},
-				u = {
-					"<cmd>FloatermUpdate<cr>",
-					"Update floaterm window attributes",
-				},
-				T = {
-					function()
-						local name = vim.fn.input("Please enter floaterm name: ")
-						vim.cmd("FloatermToggle " .. name)
-					end,
-					"Open or hide the floaterm window",
-				},
-				t = {
-					"<cmd>FloatermToggle!<cr>",
-					"Toggle all floaterm windows",
-				},
-				S = {
-					function()
-						local name = vim.fn.input("Please enter floaterm name: ")
-						vim.cmd("FloatermShow " .. name)
-					end,
-					"Show current floaterm window",
-				},
-				s = {
-					"<cmd>FloatermShow!<cr>",
-					"Show all floaterm windows",
-				},
-				H = {
-					function()
-						local name = vim.fn.input("Please enter floaterm name: ")
-						vim.cmd("FloatermHide " .. name)
-					end,
-					"Hide current floaterm window",
-				},
-				h = {
-					"<cmd>FloatermToggle!<cr>",
-					"Hide all floaterm windows",
-				},
-				K = {
-					function()
-						local name = vim.fn.input("Please enter floaterm name: ")
-						vim.cmd("FloatermKill " .. name)
-					end,
-					"Kill current floaterm window",
-				},
-				k = {
-					"<cmd>FloatermKill!<cr>",
-					"Kill all floaterm windows",
-				},
-				r = {
-					function()
-						local cmd = vim.fn.input("Please input the command to send: ")
-						vim.cmd("FLoatermSend " .. cmd)
-					end,
-					"Send(Run) command to floaterm",
-				},
-			},
+			["<leader>t"] = {},
 		})
 	end,
 }
