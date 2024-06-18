f7012837d1c364b24b4d1f8a216d213a674159a9
swahpy <hitwhy@gmail.com>
2024-06-18 22:54:41 +0800

    adjust keymaps to adapt clue

--- .config/nvim/lua/plugins/mini/ai.lua
+++ .config/nvim/lua/plugins/mini/ai.lua
@@ -44,53 +44,5 @@ return {
 				U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
 			},
 		})
-		---@type table<string, string|table>
-		local i = {
-			[" "] = "Whitespace",
-			['"'] = 'Balanced "',
-			["'"] = "Balanced '",
-			["`"] = "Balanced `",
-			["("] = "Balanced (",
-			[")"] = "Balanced ) including white-space",
-			[">"] = "Balanced > including white-space",
-			["<lt>"] = "Balanced <",
-			["]"] = "Balanced ] including white-space",
-			["["] = "Balanced [",
-			["}"] = "Balanced } including white-space",
-			["{"] = "Balanced {",
-			["?"] = "User Prompt",
-			_ = "Underscore",
-			a = "Argument",
-			b = "Balanced ), ], }",
-			c = "Class",
-			d = "Digit(s)",
-			e = "Word in CamelCase & snake_case",
-			f = "Function",
-			g = "Entire file",
-			h = "Between spaces",
-			j = "Whole line",
-			o = "Block, conditional, loop",
-			q = "Quote `, \", '",
-			t = "Tag",
-			u = "Use/call function & method",
-			U = "Use/call without dot in name",
-		}
-		local a = vim.deepcopy(i)
-		for k, v in pairs(a) do
-			---@diagnostic disable-next-line
-			a[k] = v:gsub(" including.*", "")
-		end
-
-		local ic = vim.deepcopy(i)
-		local ac = vim.deepcopy(a)
-		for key, name in pairs({ n = "Next", l = "Last" }) do
-			i[key] = vim.tbl_extend("force", { name = "Inside " .. name .. " textobject" }, ic)
-			a[key] = vim.tbl_extend("force", { name = "Around " .. name .. " textobject" }, ac)
-		end
-		require("which-key").register({
-			mode = { "o", "x" },
-			i = i,
-			a = a,
-		})
 	end,
 }
