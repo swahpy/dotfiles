33edfb98b003a8724eb5146b7e7fe796fa67113b
swahpy <hitwhy@gmail.com>
2024-06-21 17:26:13 +0800

    change keymap of triggering jump2d

--- .config/nvim/lua/plugins/mini/jump2d.lua
+++ .config/nvim/lua/plugins/mini/jump2d.lua
@@ -3,6 +3,11 @@ return {
 	version = false,
 	config = function()
 		local jump2d = require("mini.jump2d")
-		jump2d.setup()
+		jump2d.setup({
+			-- Module mappings. Use `''` (empty string) to disable one.
+			mappings = {
+				start_jumping = "<A-CR>",
+			},
+		})
 	end,
 }
