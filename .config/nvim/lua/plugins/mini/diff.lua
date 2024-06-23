d6a97e266771d98586a9d0cc502c4f39c13d7ac2
swahpy <hitwhy@gmail.com>
2024-06-23 22:53:36 +0800

    add mini plugin diff

--- .config/nvim/lua/plugins/mini/diff.lua
+++ .config/nvim/lua/plugins/mini/diff.lua
@@ -4,10 +4,14 @@ return {
 	config = function()
 		local diff = require("mini.diff")
 		diff.setup({
+			source = diff.gen_source.save(),
 			view = {
 				style = "sign",
 				signs = { add = "+", change = "~", delete = "-" },
 			},
 		})
+		vim.keymap.set("n", "<leader><leader>o", function()
+			diff.toggle_overlay()
+		end, { desc = "+mini diff toggle overlay" })
 	end,
 }
