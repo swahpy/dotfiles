893725fa5c30fa68108698a0530129655351f69f
swahpy <hitwhy@gmail.com>
2024-06-24 09:40:29 +0800

    archive nvim-surround

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -2,11 +2,4 @@ return {
 	"nvim-lua/plenary.nvim", -- lua functions that many plugis use
 	"yorickpeterse/nvim-tree-pairs", -- to fix % jump with tree-sitter
 	"RRethy/nvim-treesitter-endwise",
-	{
-		"kylechui/nvim-surround",
-		-- Use for stability; omit to use `main` branch for the latest features
-		version = "*",
-		event = "VeryLazy",
-		opts = true,
-	},
 }
