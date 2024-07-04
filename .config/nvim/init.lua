135b27a173998433457a1b45a77c531640b6defe
swahpy <hitwhy@gmail.com>
2024-07-04 14:59:57 +0800

    remove duplicated setup for mini.ai

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -206,14 +206,6 @@ later(function()
 				},
 				"^().*()$",
 			},
-			g = function() -- Whole buffer, similar to `gg` and 'G' motion
-				local from = { line = 1, col = 1 }
-				local to = {
-					line = vim.fn.line("$"),
-					col = math.max(vim.fn.getline("$"):len(), 1),
-				}
-				return { from = from, to = to }
-			end,
 			u = ai.gen_spec.function_call(), -- u for "Usage"
 			U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
 			-- from mini.extra
